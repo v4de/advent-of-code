bfbe80b40a0d917ff489ab9d3497c420158b55da
v4de <934222+v4de@users.noreply.github.com>
2024-12-03 04:01:12 -0500

    day 3 part one complete

--- 3/src/app.ts
+++ 3/src/app.ts
@@ -6,17 +6,26 @@ const readLines = createInterface({
   input: createReadStream('./src/day3.txt','utf-8'),
   terminal: false,
 });
-
+// Part Two:
+// TODO: Match only between anchors: Do() = START Don't() = END
 const instructionRe = new RegExp('mul\\([\\d]?[\\d]?[\\d]?,[\\d]?[\\d]?[\\d]?\\)','gm');
 const mulRe = new RegExp('\\d+,\\d+');
-let sum: number | undefined = 0;
+let sum: number = 0;
 
 // parse lines of stream asynchronously
 const start = async () => {
   for await (const line of readLines) {
+
+    // match mul(ddd,ddd)
     const matches = line.match(instructionRe);
+
+    // try multiplying
     const products = matches?.map((instruction:string) => {
+
+      // match ddd,ddd
       const csv = instruction.match(mulRe);
+
+      // split, map, reduce, multiply
       let multiply: number = 0;
       if (csv){
         multiply = csv[0].split(',')
@@ -25,7 +34,11 @@ const start = async () => {
       };
       return multiply;
     });
-    sum = products?.reduce((acc:number, v:number) => acc + v);
+
+    // add all multiplied products
+    if (products) {
+      sum += products?.reduce((acc:number, v:number) => acc + v);
+    }
   }
 
   // async must return a value
