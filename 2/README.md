322d5e13e7da65529122f45eafcc10d08aa0fa18
v4de <934222+v4de@users.noreply.github.com>
2024-12-03 02:30:00 -0500

    testing part 2

--- 2/README.md
+++ 2/README.md
@@ -26,12 +26,12 @@ The engineers are trying to figure out which reports are **safe**. The Red-Nosed
 
 In the example above, the reports can be found safe or unsafe by checking those rules:
 
-- 7 6 4 2 1: **Safe** because the levels are all decreasing by 1 or 2.
-- 1 2 7 8 9: **Unsafe** because 2 7 is an increase of 5.
-- 9 7 6 2 1: **Unsafe** because 6 2 is a decrease of 4.
-- 1 3 2 4 5: **Unsafe** because 1 3 is increasing but 3 2 is decreasing.
-- 8 6 4 4 1: **Unsafe** because 4 4 is neither an increase or a decrease.
-- 1 3 6 7 9: **Safe** because the levels are all increasing by 1, 2, or 3.
+- `7 6 4 2 1`: **Safe** because the levels are all decreasing by 1 or 2.
+- `1 2 7 8 9`: **Unsafe** because 2 7 is an increase of 5.
+- `9 7 6 2 1`: **Unsafe** because 6 2 is a decrease of 4.
+- `1 3 2 4 5`: **Unsafe** because 1 3 is increasing but 3 2 is decreasing.
+- `8 6 4 4 1`: **Unsafe** because 4 4 is neither an increase or a decrease.
+- `1 3 6 7 9`: **Safe** because the levels are all increasing by 1, 2, or 3.
 
 So, in this example, **2** reports are **safe**.
 
@@ -40,8 +40,38 @@ Analyze the unusual data from the engineers. **How many reports are safe?**
 <details>
   <summary>Spoiler</summary>
 
-  Your puzzle answer was ``.
+  Your puzzle answer was `321`.
 
   The first half of this puzzle is complete! It provides one gold star: *
+
+</details>
+
+## --- Part Two ---
+
+The engineers are surprised by the low number of safe reports until they realize they forgot to tell you about the Problem Dampener.
+
+The Problem Dampener is a reactor-mounted module that lets the reactor safety systems **tolerate a single bad level** in what would otherwise be a safe report. It's like the bad level never happened!
+
+Now, the same rules apply as before, except if removing a single level from an unsafe report would make it safe, the report instead counts as safe.
+
+More of the above example's reports are now safe:
+
+- `7 6 4 2 1`: **Safe** without removing any level.
+- `1 2 7 8 9`: **Unsafe** regardless of which level is removed.
+- `9 7 6 2 1`: **Unsafe** regardless of which level is removed.
+- `1 3 2 4 5`: **Safe** by removing the second level, `3`.
+- `8 6 4 4 1`: **Safe** by removing the third level, `4`.
+- `1 3 6 7 9`: **Safe** without removing any level.
+
+Thanks to the Problem Dampener, `4` reports are actually **safe**!
+
+Update your analysis by handling situations where the Problem Dampener can remove a single level from unsafe reports. **How many reports are now safe?**
+
+<details>
+  <summary>Spoiler</summary>
+
+  Your puzzle answer was ``.
+
+  Both parts of this puzzle are complete! They provide two gold stars: **
 
 </details>
--- 2/src/app.ts
+++ 2/src/app.ts
@@ -1,2 +1,104 @@
-let message: string = 'Hello World!'
-console.log(message);
+import { createReadStream } from 'fs';
+import { createInterface } from 'readline';
+
+let totalSafe: number = 0;
+
+const readLines = createInterface({
+  input: createReadStream('./src/day2.txt','utf-8'),
+  terminal: false,
+});
+
+const check = (numbers:number[]) => {
+  // setup flags
+  let ascending: boolean | undefined = undefined;
+  let descending: boolean | undefined = undefined;
+  let problemDampener: boolean | undefined = undefined;
+  let safe: boolean = true;
+
+  // loop through reports
+  for (const [k, v] of numbers.entries()) {
+
+    // do nothing if next number is undefined
+    if(k + 1 < numbers.length) {
+
+      // check if difference is less than 4 and more than 0
+      const difference = Math.abs(Number(v) - Number(numbers[k + 1]));
+      if (difference < 4 && difference > 0) {
+
+        // is it descending?
+        if (v > numbers[k + 1] && ascending === undefined && descending === undefined) {
+          descending = true;
+        }
+
+        // is it ascending?
+        if (v < numbers[k + 1] && ascending === undefined && descending === undefined) {
+          ascending = true;
+        }
+
+        // no longer descending
+        if (v < numbers[k + 1] && descending) {
+          console.log(`not descending`);
+          if (checkRemove(k, (k + 1), numbers) && problemDampener === undefined) {
+            problemDampener = true;
+          } else {
+            safe = false;
+            break;
+          }
+        }
+
+        // no longer ascending
+        if (v > numbers[k + 1] && ascending) {
+          console.log(`not ascending`);
+          if (checkRemove(k, (k + 1), numbers) && problemDampener === undefined) {
+            problemDampener = true;
+          } else {
+            safe = false;
+            break;
+          }
+        }
+      } else {
+        console.log(`difference larger than 3: ${difference}`);
+        if (checkRemove(k, (k + 1), numbers) && problemDampener === undefined) {
+          problemDampener = true;
+        } else {
+          safe = false;
+          break;
+        }
+      }
+    }
+  }
+  // async must return a value
+  return safe;
+}
+
+// checkRemove(k,k+1,numbers)
+const checkRemove = (curr:number, next:number, report:number[]) => {
+  if (check(report.splice(curr,1))) {
+    return 1;
+  } else if (check(report.splice(next,1))) {
+    return 1;
+  } else {
+    return 0;
+  }
+}
+
+// parse lines of stream asynchronously
+const start = async () => {
+  for await (const line of readLines) {
+
+    // separate each number in a report and cast to Number
+    const numbers = line.split(' ').map(number => Number(number));
+
+    if (check(numbers)) {
+      totalSafe += 1;
+    }
+  }
+
+  // async must return a value
+  return totalSafe;
+}
+
+start()
+  .then((totalSafe) => {
+    console.log(`Safe reports: ${totalSafe}`);
+  })
