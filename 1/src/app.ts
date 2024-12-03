ad153f9138774882c33c8550dce877c638d474a1
v4de <934222+v4de@users.noreply.github.com>
2024-12-02 22:47:53 -0500

    simplify part one and add comments to part two

--- 1/src/app.ts
+++ 1/src/app.ts
@@ -6,11 +6,13 @@ const list2: number[] = [];
 let distance = 0;
 let similarity = 0;
 
+// create stream to read bytes line by line
 const rl = createInterface({
   input: createReadStream('./src/day1.txt','utf-8'),
   terminal: false,
 });
 
+// parse lines of stream asynchronously
 const start = async () => {
   for await (const line of rl) {
     const numbers = line.split('   ');
@@ -18,46 +20,56 @@ const start = async () => {
     list2.push(Number(numbers[1]));
   }
 
-  return 'success';
-}
-
-start().then(message => { // part two
+  // sort number lists ascending
   list1.sort((a, b) => a - b);
   list2.sort((a, b) => a - b);
 
-  while (list1.length > 0) {
-    const smallestList1 = list1.shift()
-    const smallestList2 = list2.shift()
+  return 'success'; // promise must return something
+}
 
-    if (smallestList1 && smallestList2) {
+start() // part one
+  .then(message => {
+
+    // # of elements are identical, loop through one array and find difference of the same index
+    for (let index = 0; index < list1.length; index++) {
+      const smallestList1 = list1[index];
+      const smallestList2 = list2[index];
       distance += Math.abs(smallestList1 - smallestList2);
+      // console.log(`Debug: Distance: ${distance}`)
     }
-    console.log(`smallestList1: ${smallestList1}`);
-    console.log(`smallestList2: ${smallestList2}`);
-    console.log(`distance: ${distance}\n`);
-  }
-}).then(message => { // part two
+    console.log(`Distance: ${distance}\n`);
+    return message; // promise must return something
+}) // part two
+  .then(message => {
 
+    // filter out duplicate elements in first list
   const unique = list1.filter((v, i, self) => {
     return self.indexOf(v) === i;
   });
 
+  // for each unique, remove similar items and count, then multiply and add
   unique.forEach(item1 => {
     let count = 0;
     let index = 0;
     while (index >= 0) {
       index = list2.indexOf(item1);
       if ( index < 0 ){
-        console.log('No similar numbers found');
+        // console.log('Debug: No similar numbers found');
       } else {
         list2.splice(index, 1);
-        count += 1
+        count += 1;
       }
     }
     if (count > 0) {
-      similarity += (item1 * count)
+      similarity += (item1 * count);
+      // console.log(`Debug: Similarity: ${similarity}`);
     } else {
-      console.log(`No numbers like ${item1} on the second list`);
+      // console.log(`Debug: No numbers like ${item1} on the second list`);
     }
   });
-});
+  console.log(`Similarity: ${similarity}\n`);
+  return message; // promise must return something
+})
+  .catch((error) => {
+    console.error("Error: ", error);
+  });
